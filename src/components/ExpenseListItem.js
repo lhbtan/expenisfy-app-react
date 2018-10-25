c19b449f590b5bf51a1f310a52feb94129da58c8
LAPTOP-3702T49L\Lloyd H.B. Tan <l.h.b.tan@gmail.com>
2018-10-25 12:18:53 +0200

    status before numeral crash

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, NavLink, Link} from 'react-router-dom';
+import moment from 'moment';
+
 
 const ExpenseListItem = ( { id, description, amount, createdAt }) => (
     <div>
     <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-        <p>Amount: {amount}, Created At: {createdAt}</p>       
+        <p>
+        Amount: {amount}, 
+        Created At: {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>       
     </div>
 );
 
