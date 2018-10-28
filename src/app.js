c578d6bf5791ac4539dfe829d9b2d21d0067a726
Lloyd <l.h.b.tan@gmail.com>
2018-10-28 23:11:29 +0100

    setstartexpenses added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses'
+import { startSetExpenses, removeExpense, editExpense } from './actions/expenses'
 import * as actionFilters from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -21,4 +21,10 @@ const jsx = (
 )
 
 
-ReactDOM.render(jsx , document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p> , document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=> {
+    ReactDOM.render(jsx , document.getElementById('app'));
+})
+
+
--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -3,19 +3,19 @@ import moment from 'moment';
 
 export default [
     {
-        id: 1,
+        id: '1',
         description: 'gum',
         note: 'note 1',
         amount: 123,
         createdAt: 0
     },{
-        id: 2,
+        id: '2',
         description: 'rent',
         note: '',
         amount: 2223,
         createdAt: moment(0).subtract(4, 'days').valueOf()  
     },{
-        id: 3,
+        id: '3',
         description: 'credit card',
         note: '',
         amount: 12345,
